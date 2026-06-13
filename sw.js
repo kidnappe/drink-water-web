c95d70dd45544b6744c858c691efd692ec57fbcc
kidnappe <123857738+kidnappe@users.noreply.github.com>
2026-06-13 13:05:29 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,2 @@
+﻿self.addEventListener('install', function() { self.skipWaiting(); });
+self.addEventListener('activate', function(e) { e.waitUntil(clients.claim().then(function() { return caches.keys().then(function(names) { return Promise.all(names.map(function(n) { return caches.delete(n); })); }); })); });

4df0585b0a067a689658782123c042aab8bfc02d
zhouxuan <2037263090@qq.com>
2026-06-17 05:01:07 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,10 @@
-var CACHE_NAME = 'drink-water-v1';
-self.addEventListener('install', function() {
-  self.skipWaiting();
-});
+self.addEventListener('install', function() { self.skipWaiting(); });
 self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    Promise.all([
-      clients.claim(),
-      caches.keys().then(function(keys) {
-        return Promise.all(keys.map(function(k) { return caches.delete(k); }));
-      })
-    ])
-  );
+  e.waitUntil(Promise.all([
+    clients.claim(),
+    caches.keys().then(function(keys) { return Promise.all(keys.map(function(k) { return caches.delete(k); })); })
+  ]));
 });
 self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    fetch(e.request).catch(function() { return caches.match(e.request); })
-  );
+  e.respondWith(fetch(e.request).catch(function() { return new Response('', { status: 200 }); }));
 });

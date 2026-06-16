58253051c0f640930c7983d6f2bc190c3839b7ad
zhouxuan <2037263090@qq.com>
2026-06-17 06:02:38 +0800

    修复sw错误

--- sw.js
+++ sw.js
@@ -1,10 +1,16 @@
 self.addEventListener('install', function() { self.skipWaiting(); });
 self.addEventListener('activate', function(e) {
-  e.waitUntil(Promise.all([
-    clients.claim(),
-    caches.keys().then(function(keys) { return Promise.all(keys.map(function(k) { return caches.delete(k); })); })
-  ]));
+  e.waitUntil(
+    Promise.all([
+      clients.claim(),
+      caches.keys().then(function(keys) {
+        return Promise.all(keys.map(function(k) { return caches.delete(k); }));
+      })
+    ])
+  );
 });
 self.addEventListener('fetch', function(e) {
-  e.respondWith(fetch(e.request).catch(function() { return new Response('', { status: 200 }); }));
+  e.respondWith(
+    fetch(e.request).catch(function() { return new Response('', { status: 200 }); })
+  );
 });
